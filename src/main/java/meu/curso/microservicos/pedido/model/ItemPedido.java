3794276a0a0d1fc0777c7e344de41304e78a084f
CaiquePithon <caiquepithon2005@gmail.com>
2026-05-18 21:14:28 -0300

    Configurações de dependencias e model config

---
+++ src/main/java/meu/curso/microservicos/pedido/model/ItemPedido.java
@@ -0,0 +1,26 @@
+package meu.curso.microservicos.pedido.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import jakarta.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "itens_pedido")
+public class ItemPedido {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private int quantidade;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+}
---
+++ src/main/java/meu/curso/microservicos/pedido/model/Pedido.java
@@ -0,0 +1,26 @@
+package meu.curso.microservicos.pedido.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import jakarta.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    private String descricao;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<ItemPedido> itens;
+}
